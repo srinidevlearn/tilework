dc57e820516153bd9b05081cb8b90a13805f138e
srinidevlearn <65352160+srinidevlearn@users.noreply.github.com>
2021-06-24 22:09:00 +0530

    migrated ng to 12
    migrated ng to 12

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CaroselComponent } from './carosel/carosel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    HeaderComponent,
+    FooterComponent,
+    CaroselComponent
   ],
   imports: [
     BrowserModule,
